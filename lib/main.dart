79cc707bbad6efc79e0f19e70101fcd82f781175
rwasei74 <r.khamira@uot.edu.ly>
2026-03-19 01:30:42 +0200

    + Initialize project. + Added empty home screen

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:islamy_app/ui/home/home_screen.dart';
+import 'package:islamy_app/utils/app_routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute:AppRoutes.homeRouteName ,
+      routes:{ AppRoutes.homeRouteName :(context) => HomeScreen() ,
+      }
+    );
+
+}
